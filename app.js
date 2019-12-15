d348d24ade71239b584619c10acc3dabfbc7016b
kaneki666 <41444793+kaneki666@users.noreply.github.com>
2019-12-15 12:49:49 +0600

    cd

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ app.use(cors());
 
 
 //Routes
-router.get('/', (req, res) => res.render('menu'));
-router.get('/Media', (req, res) => res.render('media'));
-router.get('/Products', (req, res) => res.render('allproduct'));
-router.get('/Torrent', (req, res) => res.render('torrent'));
+app.get('/', (req, res) => res.render('menu'));
+app.get('/Media', (req, res) => res.render('media'));
+app.get('/Products', (req, res) => res.render('allproduct'));
+app.get('/Torrent', (req, res) => res.render('torrent'));
 
 
 
-router.get("/searchmedia", async(req, res) => {
+app.get("/searchmedia", async(req, res) => {
     const { query } = req.query;
     const Media2 = await mediascraper.search2(query);
     const Media1 = await mediascraper.search1(query);
@@ -48,7 +48,7 @@ router.get("/searchmedia", async(req, res) => {
     res.render('media', { allProducts });
 });
 
-router.get("/searchmediajson", async(req, res) => {
+app.get("/searchmediajson", async(req, res) => {
     const { query } = req.query;
     const Media2 = await mediascraper.search2(query);
     const Media1 = await mediascraper.search1(query);
@@ -59,7 +59,7 @@ router.get("/searchmediajson", async(req, res) => {
 });
 
 
-router.get("/searchproducts", async(req, res) => {
+app.get("/searchproducts", async(req, res) => {
     const { query } = req.query;
     const cellBazarProducts = await scraper.searchProducts1(query);
     const bikroyDotComProducts = await scraper.searchProducts2(query);
@@ -68,7 +68,7 @@ router.get("/searchproducts", async(req, res) => {
     res.render('allproduct', { allProducts });
 });
 
-router.get("/searchproductsjson", async(req, res) => {
+app.get("/searchproductsjson", async(req, res) => {
     const { query } = req.query;
     const cellBazarProducts = await scraper.searchProducts1(query);
     const bikroyDotComProducts = await scraper.searchProducts2(query);
@@ -79,7 +79,7 @@ router.get("/searchproductsjson", async(req, res) => {
 
 
 
-router.get("/searchtorrent", async(req, res) => {
+app.get("/searchtorrent", async(req, res) => {
     const { query } = req.query;
     const piratebay = await torrentscraper.searchPiratebay(query);
     const Torrentz2 = await torrentscraper.searchTorrentz2(query);
@@ -87,7 +87,7 @@ router.get("/searchtorrent", async(req, res) => {
     const allTorrent = [...piratebay, ...Torrentz2];
     res.render('torrent', { allTorrent });
 });
-router.get("/searchtorrentjson", async(req, res) => {
+app.get("/searchtorrentjson", async(req, res) => {
     const { query } = req.query;
     const piratebay = await torrentscraper.searchPiratebay(query);
     const Torrentz2 = await torrentscraper.searchTorrentz2(query);
@@ -96,7 +96,7 @@ router.get("/searchtorrentjson", async(req, res) => {
     res.json(allTorrent);
 });
 
-router.get("/newsjson", async(req, res) => {
+app.get("/newsjson", async(req, res) => {
     const bbcBangla = await newsscraper.bbcBangla();
     const prothomAlo = await newsscraper.prothomAlo();
     const dhakaTribune = await newsscraper.dhakaTribune();
@@ -104,7 +104,7 @@ router.get("/newsjson", async(req, res) => {
     res.json(allNews);
     
 });
-router.get("/topgoaljson", async(req, res) => {
+app.get("/topgoaljson", async(req, res) => {
     const topstat = await footballscraper.topStats();
     const reststat = await  footballscraper.restStats();
     const top = await top.slice(0,1);
@@ -114,7 +114,7 @@ router.get("/topgoaljson", async(req, res) => {
     res.json(all);
 });
 
-router.get("/topassistjson", async(req, res) => {
+app.get("/topassistjson", async(req, res) => {
     const topstat = await footballscraper.topStats();
     const reststat = await  footballscraper.restStats();
     const top = await topstat.slice(1,2);
@@ -124,7 +124,7 @@ router.get("/topassistjson", async(req, res) => {
     res.json(all);
 })
 
-router.get("/toppassesjson", async(req, res) => {
+app.get("/toppassesjson", async(req, res) => {
     const topstat = await footballscraper.topStats();
     const reststat = await  footballscraper.restStats();
     const top = await topstat.slice(2,3);
@@ -134,7 +134,7 @@ router.get("/toppassesjson", async(req, res) => {
     res.json(all);
 });
 
-router.get("/cleansheetsjson", async(req, res) => {
+app.get("/cleansheetsjson", async(req, res) => {
     const topstat = await footballscraper.topStats();
     const reststat = await  footballscraper.restStats();
     const top = await topstat.slice(3,4);
@@ -144,7 +144,7 @@ router.get("/cleansheetsjson", async(req, res) => {
     res.json(all);
 });
 
-router.get("/topgoalteamjson", async(req, res) => {
+app.get("/topgoalteamjson", async(req, res) => {
 
     const topstat = await footballscraper.topStats();
     const reststat = await  footballscraper.restStats();
@@ -155,7 +155,7 @@ router.get("/topgoalteamjson", async(req, res) => {
     res.json(all);
 });
 
-router.get("/toppassesteamjson", async(req, res) => {
+app.get("/toppassesteamjson", async(req, res) => {
     const topstat = await footballscraper.topStats();
     const reststat = await  footballscraper.restStats();
     const top = await topstat.slice(5,6);
@@ -165,7 +165,7 @@ router.get("/toppassesteamjson", async(req, res) => {
     res.json(all);
 });
 
-router.get("/topcleansheetsjsonteamjson", async(req, res) => {
+app.get("/topcleansheetsjsonteamjson", async(req, res) => {
     const topstat = await footballscraper.topStats();
     const reststat = await  footballscraper.restStats();
     const top = await topstat.slice(6,7);
@@ -175,7 +175,7 @@ router.get("/topcleansheetsjsonteamjson", async(req, res) => {
     res.json(all);
 });
 
-router.get("/topcrossesteamjson", async(req, res) => {
+app.get("/topcrossesteamjson", async(req, res) => {
     const topstat = await footballscraper.topStats();
     const reststat = await  footballscraper.restStats();
     const top = await topstat.slice(7,8);
