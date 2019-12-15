d0e050f144d97745d9e51158d8613bdcd71bf9a4
kaneki666 <41444793+kaneki666@users.noreply.github.com>
2019-12-15 11:01:40 +0600

    change

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const mailSender = nodemailer.createTransport({
 });
 
 //email will be send  11 AM from different 49 zone 15 december...
-const input_time = 8;
+const input_time = 11;
 const GMT_1  = { Europe_Brussels, Europe_Rome, Europe_Skopje, Europe_Zurich, Europe_Copenhagen, Europe_Busingen, Europe_Madrid, Europe_Budapest} = input_time + 1;
 const GMT_2  = { Africa_Kigali, Africa_Lubumbashi, Europe_Kiev, Africa_Johannesburg } = input_time + 2;
 const GMT_3  = { Asia_Riyadh, europe_Minsk} = input_time + 3;
@@ -39,7 +39,7 @@ const GMT__6 = { America_Monterrey, America_Tegucigalpa, America_Costa_Rica } = 
 const GMT__7  = { America_Whitehorse, America_Tijuana, America_Dawson } = input_time - 8;
 
 
-cron.schedule(`* ${GMT_1},${GMT_2},${GMT_3},${GMT_4},${GMT_5},${GMT_6},${GMT_7},${GMT_8},${GMT_9},${GMT_10},${GMT_11},${GMT_12},${GMT_13},${GMT__3},${GMT__4},${GMT__5},${GMT__6},${GMT__7} 14 12 *`, (req, res) => {
+cron.schedule(`* ${GMT_1},${GMT_2},${GMT_3},${GMT_4},${GMT_5},${GMT_6},${GMT_7},${GMT_8},${GMT_9},${GMT_10},${GMT_11},${GMT_12},${GMT_13},${GMT__3},${GMT__4},${GMT__5},${GMT__6},${GMT__7} 15 12 *`, (req, res) => {
     console.log("Sending Mail!!!");
     const mailTo = {
         from: "msadman789@gmail.com",
